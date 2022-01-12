8e4dfc3e2316654992999185bc4f4a2537679bb4
2YoungHoon <djaakclsrn12@naver.com>
2022-01-12 16:26:55 +0900

    init commit

---
+++ src/test/java/com/mobileinvitation/mobileinvitation/MobileInvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileinvitation.mobileinvitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileInvitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
