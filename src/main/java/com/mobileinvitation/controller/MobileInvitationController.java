ede39fab01cdc36ff284c6ae52c5da2c8f24b1ca
2YoungHoon <djaakclsrn12@naver.com>
2022-01-17 23:35:15 +0900

    Controller Test 작성

--- src/main/java/com/mobileinvitation/controller/MobileInvitationController.java
+++ src/main/java/com/mobileinvitation/controller/MobileInvitationController.java
@@ -1,4 +1,34 @@
 package com.mobileinvitation.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
 public class MobileInvitationController {
+
+
+    @GetMapping("/path/{userName}/{userPass}")
+    public String TestPath(@PathVariable String userName, @PathVariable String userPass) {
+
+        log.info("userName: {}", userName);
+        log.info("userPass: {}", userPass);
+
+        return "pathsucess";
+    }
+
+    @GetMapping("/req/{userName}/{userPass}")
+    public String TestReq(@RequestParam String userName, @RequestParam String userPass) {
+
+        log.info("userName: {}", userName);
+        log.info("userPass: {}", userPass);
+
+        return "reqsucess";
+    }
+
+
 }
