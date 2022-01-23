ba21648079bd131dd6d85e548cacfc768c15f5c5
seonji0326 <seonji0326@gmail.com>
2022-01-23 23:00:24 +0900

    화면 샘플...

--- src/main/java/com/mobileinvitation/controller/MobileInvitationController.java
+++ src/main/java/com/mobileinvitation/controller/MobileInvitationController.java
@@ -1,24 +1,17 @@
 package com.mobileinvitation.controller;
 
-import com.mobileinvitation.model.request.CreateUserReq;
-import com.mobileinvitation.service.MobileInvitationService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequiredArgsConstructor
-@Slf4j
+@Controller
 public class MobileInvitationController {
 
-    private final MobileInvitationService mobileInvitationService;
-
-    @PostMapping("/")
-    public String login(@RequestBody CreateUserReq createUserReq) throws Exception {
-
-        String result = mobileInvitationService.savePost(createUserReq);
-
-        return result;
+    @GetMapping(value = "/information")
+    public ModelAndView information() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("information");
+        return modelAndView;
     }
+
 }
---
+++ src/main/java/com/mobileinvitation/controller/MobileInvitationRestController.java
@@ -0,0 +1,23 @@
+package com.mobileinvitation.controller;
+
+import com.mobileinvitation.model.request.CreateUserReq;
+import com.mobileinvitation.service.MobileInvitationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class MobileInvitationRestController {
+
+    private final MobileInvitationService mobileInvitationService;
+
+    @PostMapping("/")
+    public String login(@RequestBody CreateUserReq createUserReq) throws Exception {
+
+        String result = mobileInvitationService.savePost(createUserReq);
+
+        return result;
+    }
+}
