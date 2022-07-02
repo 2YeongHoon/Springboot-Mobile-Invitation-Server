d994ab493f38ad463ec17ad5f5298b18933538a4
2YoungHoon <djaakclsrn12@naver.com>
2022-07-02 21:26:51 +0900

    CD 실행 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE 1>/dev/null 2>&1 &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
