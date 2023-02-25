8fec2f8bca50acb380d4c3b609912df74a162aaf
2Yeonghoon <djaakclsrn12@naver.com>
2023-02-25 23:42:51 +0900

    IMP: 웨딩정보 추가

--- src/main/java/com/mobileinvitation/invitation/dto/WeddingInfoRequest.java
+++ src/main/java/com/mobileinvitation/invitation/dto/WeddingInfoRequest.java
@@ -2,21 +2,12 @@ package com.mobileinvitation.invitation.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.io.Serializable;
-import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class WeddingInfoRequest implements Serializable {
-
-  @NotBlank(message = "사용자 핸드폰번호를 입력해 주세요.")
-  @Schema(description = "사용자 핸드폰번호", required = true)
-  private String phone;
-
-  @NotBlank(message = "사용자 비밀번호를 입력해 주세요.")
-  @Schema(description = "사용자 비밀번호")
-  private String password;
 
   @Schema(description = "신랑이름")
   private String manName;
