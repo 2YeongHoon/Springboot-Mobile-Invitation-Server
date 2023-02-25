fb7e7f547509961daaecad697b32fc08ccfcab6a
2Yeonghoon <djaakclsrn12@naver.com>
2023-02-25 23:43:01 +0900

    IMP: 미사용 임포트 삭제

--- src/main/java/com/mobileinvitation/config/aws/s3/S3UploaderService.java
+++ src/main/java/com/mobileinvitation/config/aws/s3/S3UploaderService.java
@@ -1,20 +1,14 @@
 package com.mobileinvitation.config.aws.s3;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.mobileinvitation.invitation.entity.ImageEntity;
 import com.mobileinvitation.invitation.entity.VideoEntity;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Component
