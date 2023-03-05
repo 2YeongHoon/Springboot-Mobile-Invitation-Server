db37f12ec66e5698344d8bc4272f4788a6a07539
2Yeonghoon <djaakclsrn12@naver.com>
2023-03-05 23:36:29 +0900

    IMP: swagger정보변경

--- src/main/java/com/mobileinvitation/config/swagger/SwaggerConfig.java
+++ src/main/java/com/mobileinvitation/config/swagger/SwaggerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
@@ -32,43 +33,43 @@ public class SwaggerConfig {
 //    @Value("${custom.title}")
 //    private String TITLE;
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-            .apiInfo(apiInfo())
-            .pathMapping("/")
-            .securityContexts(Collections.singletonList(securityContext()))
-            .securitySchemes(Collections.singletonList(apiKey()))
-            .useDefaultResponseMessages(false)
-            .select()
-            .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-//            .apis(RequestHandlerSelectors.any())
-//            .paths(PathSelectors.any())
-            .build();
-    }
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.OAS_30)
+        .apiInfo(apiInfo())
+        .pathMapping("/")
+        .securityContexts(Collections.singletonList(securityContext()))
+        .securitySchemes(Collections.singletonList(apiKey()))
+        .useDefaultResponseMessages(false)
+        .select()
+        .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
 
-    private ApiKey apiKey() {
-        return new ApiKey("Authorization", "Bearer", "header");
-    }
+  private ApiKey apiKey() {
+    return new ApiKey("Authorization", "Bearer", "header");
+  }
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth()).build();
-    }
+  private SecurityContext securityContext() {
+    return SecurityContext.builder().securityReferences(defaultAuth()).build();
+  }
 
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global",
-            "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return List.of(new SecurityReference("Authorization", authorizationScopes));
-    }
+  List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global",
+        "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return List.of(new SecurityReference("Authorization", authorizationScopes));
+  }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("Title")
-            .description("Connect doctors and patients")
-            .license("Ikoob. All rights reserved")
-            .version("1.0.0")
-            .build();
-    }
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("Title")
+        .description("Connect doctors and patients")
+        .license("Ikoob. All rights reserved")
+        .version("1.0.0")
+        .build();
+  }
 }
