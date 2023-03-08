c10913448cbf60d86487dcf42dceb5a3b879b3ba
2Yeonghoon <djaakclsrn12@naver.com>
2023-03-08 23:22:32 +0900

    BUG: swagger 3.0.0, boot 2.6.2 충돌이슈 해결

--- src/main/java/com/mobileinvitation/MobileInvitationApplication.java
+++ src/main/java/com/mobileinvitation/MobileInvitationApplication.java
@@ -3,12 +3,14 @@ package com.mobileinvitation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 @EnableJpaAuditing
 public class MobileInvitationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MobileInvitationApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MobileInvitationApplication.class, args);
+  }
 }
